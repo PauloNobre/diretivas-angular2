ecc3fe49a6d852f930c83877926fed34ca0fbb9c
PauloNobre <paulinho140@gmail.com>
2019-03-08 08:40:35 -0300

    25: Diretivas: ngIf

--- src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos: string[] = ["Angular 2"];
+
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggleCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
